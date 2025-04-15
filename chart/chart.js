cb8d3a85a4e82860c5cc67a6e1faef7c83ef6faa
Levente Szűcs <45243823+GohanFX@users.noreply.github.com>
2025-04-15 13:55:08 +0200

    move table drawing into js file

--- chart/chart.js
+++ chart/chart.js
@@ -1,3 +1,11 @@
+for (let i = 0; i < 5; i++) {
+  document.write('<tr onclick="drawChart(this)">');
+  for (let j = 0; j < 5; j++) {
+    document.write(`<td>${Math.floor(Math.random() * 100)}</td>`);
+  }
+  document.write("</tr>");
+}
+
 let chart;
 function drawChart(row) {
   let values = Array.from(row.children).map((td) => parseInt(td.innerText));
